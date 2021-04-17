a8f967a50b6bf2fd4792276a1b1880f4daedebd2
alstelmach <73331704+alstelmach@users.noreply.github.com>
2021-04-17 18:17:19 +0200

    Moved few files, added entity id protected setter (#7)
    * Created core solution
    
    * Implemented core domain
    
    * Implemented core application layer
    
    * Implemented core infrastructure and utilities
    
    * Updated readme.md file
    
    * Made some changes to aggregate root
    
    * Renamed identity provider interface
    
    * Deleted unnecessary files
    
    * Added Marten support
    
    * Implemented base event store
    
    * Made some changes to event store
    
    * Fixed read model repository
    
    * Implemented jwt validation
    
    * Added JwtDecoder class
    
    * Fixed few bugs
    
    * Renamed project
    
    * Renamed projects and namespaces
    
    * Added nuget package information
    
    * Tested renaming
    
    * Restored naming after test
    
    * Updated README.md
    
    * Deleted invalid files
    
    * Added protected setter to entity and moved some files
    
    Co-authored-by: Aleksander Stelmach <astelmach@ess.etisoft.pl>

---
+++ AsCore.Application.Abstractions/Identity/ClaimTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace AsCore.Application.Abstractions.Identity
+{
+    public static class ClaimTypes
+    {
+        public const string OwnerId = "OwnerId";
+    }
+}
---
+++ AsCore.Application.Abstractions/Identity/ClaimsPrincipalExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace AsCore.Application.Abstractions.Identity
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static Guid GetOwnerId(this ClaimsPrincipal claimsPrincipal) =>
+            Guid.Parse(claimsPrincipal
+                 .Claims
+                 .FirstOrDefault(claim =>
+                     claim.Type == ClaimTypes.OwnerId)
+                 ?.Value
+                 ?? string.Empty);
+    }
+}
