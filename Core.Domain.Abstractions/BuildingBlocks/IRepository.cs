68887641cd4055fc0a7ebe7f18ada329f6b4170b
Aleksander Stelmach <astelmach@ess.etisoft.pl>
2021-04-17 12:47:53 +0200

    Deleted invalid files

--- Core.Domain.Abstractions/BuildingBlocks/IRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Core.Domain.Abstractions.BuildingBlocks
-{
-    public interface IRepository<in TAggregateRoot> where TAggregateRoot : AggregateRoot
-    {
-        Task CommitAsync(TAggregateRoot aggregateRoot);
-    }
-}
