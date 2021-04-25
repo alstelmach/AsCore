818479c9da00dd5acac14da4785451dcf7ceb7c3
alstelmach <73331704+alstelmach@users.noreply.github.com>
2021-04-25 15:36:35 +0200

    Added CORS support (#10)
    * Created core solution
    
    * Implemented core domain
    
    * Implemented core application layer
    
    * Implemented core infrastructure and utilities
    
    * Updated readme.md file
    
    * Made some changes to aggregate root
    
    * Renamed identity provider interface
    
    * Deleted unnecessary files
    
    * Added Marten support
    
    * Implemented base event store
    
    * Made some changes to event store
    
    * Fixed read model repository
    
    * Implemented jwt validation
    
    * Added JwtDecoder class
    
    * Fixed few bugs
    
    * Renamed project
    
    * Renamed projects and namespaces
    
    * Added nuget package information
    
    * Tested renaming
    
    * Restored naming after test
    
    * Updated README.md
    
    * Deleted invalid files
    
    * Added protected setter to entity and moved some files
    
    * Clean up versions
    
    * Clean up versions again
    
    * Added cors support to infrastructure
    
    Co-authored-by: Aleksander Stelmach <astelmach@ess.etisoft.pl>

--- AsCore.Infrastructure/Mvc/MvcConfiguration.cs
+++ AsCore.Infrastructure/Mvc/MvcConfiguration.cs
@@ -1,6 +1,8 @@
-﻿using AsCore.Infrastructure.Mvc.Filters;
+﻿using AsCore.Infrastructure.Mvc.Cors;
+using AsCore.Infrastructure.Mvc.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 
@@ -9,25 +11,33 @@ namespace AsCore.Infrastructure.Mvc
     public static class MvcConfiguration
     {
         public static IServiceCollection AddMvcDependencies(this IServiceCollection services,
+            IConfiguration configuration,
             int majorVersion,
-            int minorVersion) =>
-                services
-                    .AddControllers(options => options
-                        .Filters
-                        .Add<ExceptionFilter>())
-                    .AddNewtonsoftJson(options =>
-                        options
-                            .SerializerSettings
-                            .ReferenceLoopHandling = ReferenceLoopHandling.Ignore)
-                    .Services
-                    .AddApiVersioning(options =>
-                    {
-                        options.DefaultApiVersion = new ApiVersion(majorVersion, minorVersion);
-                        options.AssumeDefaultVersionWhenUnspecified = true;
-                        options.ReportApiVersions = true;
-                    });
+            int minorVersion,
+            bool addCors = false)
+        {
+            services
+                .AddControllers(options => options
+                    .Filters
+                    .Add<ExceptionFilter>())
+                .AddNewtonsoftJson(options =>
+                    options
+                        .SerializerSettings
+                        .ReferenceLoopHandling = ReferenceLoopHandling.Ignore)
+                .Services
+                .AddApiVersioning(options =>
+                {
+                    options.DefaultApiVersion = new ApiVersion(majorVersion, minorVersion);
+                    options.AssumeDefaultVersionWhenUnspecified = true;
+                    options.ReportApiVersions = true;
+                });
 
-        public static IApplicationBuilder UseEndpointsMiddleware(this IApplicationBuilder applicationBuilder) =>
+            return addCors
+                ? services.RegisterCorsDependencies(configuration)
+                : services;
+        }
+
+        public static IApplicationBuilder UseMvcMiddlewares(this IApplicationBuilder applicationBuilder) =>
             applicationBuilder
                 .UseEndpoints(builder =>
                     builder.MapControllers());
