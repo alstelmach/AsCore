7505d7ab86ec9f04a95d5a32454237ef2d2fb319
Aleksander Stelmach <astelmach@ess.etisoft.pl>
2021-05-02 23:16:31 +0200

    Replaced obsolete ExceptionFilterAttribute with IExceptionFilter and marked exception handled flag

--- AsCore.Infrastructure/Mvc/Filters/ExceptionFilter.cs
+++ AsCore.Infrastructure/Mvc/Filters/ExceptionFilter.cs
@@ -1,39 +1,40 @@
 ﻿using System;
 using System.Net;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Newtonsoft.Json;
 
 namespace AsCore.Infrastructure.Mvc.Filters
 {
-    public sealed class ExceptionFilter : ExceptionFilterAttribute
+    public sealed class ExceptionFilter : IExceptionFilter
     {
         private const string ResponseContentType = "application/json";
 
-        public override void OnException(ExceptionContext exceptionContext)
+        public void OnException(ExceptionContext exceptionContext)
         {
             var response = exceptionContext.HttpContext.Response;
             var message = exceptionContext.Exception.Message;
             var statusCode = FindCorrespondingStatusCode(exceptionContext.Exception);
             
-            CreateResponseAsync(response, message, statusCode).Wait();
+            CreateResponse(response, message, statusCode);
+            exceptionContext.ExceptionHandled = true;
         }
         
-        private static async Task CreateResponseAsync(HttpResponse response,
+        private static void CreateResponse(HttpResponse response,
             string errorMessage,
-            HttpStatusCode statusCode)
+            HttpStatusCode httpStatusCode)
         {
-            var result = JsonConvert.SerializeObject(
+            var statusCode = (int) httpStatusCode;
+            var body = JsonConvert.SerializeObject(
                 new
                 {
                     Message = errorMessage,
-                    StatusCode = (int) statusCode,
+                    StatusCode = statusCode,
                 });
 
-            response.StatusCode = (int) statusCode;
+            response.StatusCode = statusCode;
             response.ContentType = ResponseContentType;
-            await response.WriteAsync(result);
+            response.WriteAsync(body).Wait();
         }
 
         private static HttpStatusCode FindCorrespondingStatusCode(Exception exception) =>
