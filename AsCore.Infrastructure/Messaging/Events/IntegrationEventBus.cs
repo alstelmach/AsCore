92ca3bc3cc8997210e319af9a436d57cafecb1ea
Aleksander Stelmach <astelmach@ess.etisoft.pl>
2021-06-05 11:50:14 +0200

    Divided EventBus

---
+++ AsCore.Infrastructure/Messaging/Events/IntegrationEventBus.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using AsCore.Application.Abstractions.Messaging.Events;
+using MassTransit;
+
+namespace AsCore.Infrastructure.Messaging.Events
+{
+    public sealed class IntegrationEventBus : IIntegrationEventPublisher
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public IntegrationEventBus(IPublishEndpoint publishEndpoint) =>
+            _publishEndpoint = publishEndpoint;
+
+        public async Task PublishAsync<TIntegrationEvent>(TIntegrationEvent @event)
+            where TIntegrationEvent : IntegrationEvent
+        {
+            @event.MarkAsPublished();
+            await _publishEndpoint.Publish(@event);
+        }
+    }
+}
