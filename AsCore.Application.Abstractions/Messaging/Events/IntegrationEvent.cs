56fe7471617aee8ba8cfe9e9364b212ed5081cde
olekstelmach <aleksander.stelmach.ext@talabat.com>
2021-12-19 17:03:23 +0100

    Removed entity id from integration event

--- AsCore.Application.Abstractions/Messaging/Events/IntegrationEvent.cs
+++ AsCore.Application.Abstractions/Messaging/Events/IntegrationEvent.cs
@@ -4,11 +4,7 @@ namespace AsCore.Application.Abstractions.Messaging.Events
 {
     public abstract record IntegrationEvent
     {
-        protected IntegrationEvent(object entityId) =>
-            EntityId = entityId;
-
         public Guid Id { get; } = Guid.NewGuid();
-        public object EntityId { get; }
         public DateTime? PublishedAtUtc { get; set; }
     }
 }
